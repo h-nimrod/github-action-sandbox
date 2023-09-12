ab44c0d5dae4d832fa48f4a96edbfb3e32cae75a
h-nimrod <h.tanabe@gmail.com>
2023-09-12 13:18:51 +0900

    show snackbar

--- android-example/app/src/main/java/com/hnimrod/example/MainActivity.kt
+++ android-example/app/src/main/java/com/hnimrod/example/MainActivity.kt
@@ -3,12 +3,14 @@ package com.hnimrod.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +27,12 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background,
                 ) {
                     Box(contentAlignment = Alignment.Center) {
-                        Greeting("Android")
+                        val snackbarHostState = remember { SnackbarHostState() }
+                        SnackbarHost(
+                            hostState = snackbarHostState,
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        )
+                        Greeting("Android", snackbarHostState)
                     }
                 }
             }
@@ -34,17 +41,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, snackbarHostState: SnackbarHostState, modifier: Modifier = Modifier) {
+    val text = "Hello $name!"
+    val showSnackbar = remember { mutableStateOf(false) }
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier,
+        text = text,
+        modifier = modifier.clickable {
+            showSnackbar.value = true
+        }
     )
+
+    if (showSnackbar.value) {
+        LaunchedEffect(snackbarHostState) {
+            snackbarHostState.showSnackbar(message = text)
+            showSnackbar.value = false
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidExampleTheme {
-        Greeting("Android")
+        Greeting("Android", SnackbarHostState())
     }
 }
